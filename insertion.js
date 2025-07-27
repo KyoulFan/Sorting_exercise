e87dcf6626dbdaafbd4564e57b3d7a9387aef6a0
Ruth <ruthleeyuxin@gmail.com>
2025-07-27 16:57:24 -0700

    add new comment on pointer i

--- insertion.js
+++ insertion.js
@@ -2,12 +2,13 @@
 // and use pointer j to find from right to left
 function insertionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
+    //control point i, must go through all the elements in the array
     let current = arr[i];
     let j = i - 1;
 
     while (j >= 0 && arr[j] > current) {
-      arr[j + 1] = arr[j];
-      j--;
+      arr[j + 1] = arr[j]; //keep rewrite number
+      j--; //move to left
     }
     arr[j + 1] = current;
   }
