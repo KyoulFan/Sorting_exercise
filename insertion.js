a7ead5eec6dff01db91894bf064e9f1de8c9185c
Ruth <ruthleeyuxin@gmail.com>
2025-07-27 16:29:47 -0700

    Fixed by ChatGPT

--- insertion.js
+++ insertion.js
@@ -1,9 +1,11 @@
-function insertionSort() {
+function insertionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let current = arr[i];
+    let j = i - 1;
 
-    for (let j = i - 1; j >= 0 && arr[j] > current; j--) {
+    while (j >= 0 && arr[j] > current) {
       arr[j + 1] = arr[j];
+      j--;
     }
     arr[j + 1] = current;
   }
