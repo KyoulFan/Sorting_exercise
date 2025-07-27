7ea329942fd6c34b27b2290eedd2f781e49a06c9
Ruth <ruthleeyuxin@gmail.com>
2025-07-27 16:02:59 -0700

    add insertion

---
+++ insertion.js
@@ -0,0 +1,13 @@
+function insertionSort() {
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+
+    for (let j = i - 1; j >= 0 && arr[j] > current; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+
+module.exports = insertionSort;
